d7aea3960e90a8d535e0cec7022743321b7e4ae7
Brandon Donato-Long <wartornhero@gmail.com>
2017-04-16 01:14:41 -0700

    Brought in an express example for RestAPI

--- index.js
+++ index.js
@@ -5,5 +5,43 @@ var ObjectiveList = require('./src/ObjectiveList');
 var Objective = require('./src/Objective');
 var User = require('./src/User');
 var UserList = require('./src/UserList');
+var Accomplishment = require('./src/Accomplishments');
+var AccomplishmentList = require('./src/AccomplishmentList');
 
-//moved all the stuff that was here to a test folder.
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/User', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
+//Here create a REST Api to use.
+
+
